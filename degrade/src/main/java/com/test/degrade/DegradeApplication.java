b9ea87b45c7359939b37bc74421d31ecee0e744d
ikaruga <nobody@nobody.com>
2023-10-12 19:53:46 +0800

    init

---
+++ degrade/src/main/java/com/test/degrade/DegradeApplication.java
@@ -0,0 +1,13 @@
+package com.test.degrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DegradeApplication.class, args);
+    }
+
+}
---
+++ degrade/src/test/java/com/test/degrade/DegradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.degrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
